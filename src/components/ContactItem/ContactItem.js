9371170e611f05012b7c80045aa7656544759008
OsokinNikita <110087045+OsokinNikita@users.noreply.github.com>
2023-04-06 10:28:16 +0300

    apgrade

--- src/components/ContactItem/ContactItem.js
+++ src/components/ContactItem/ContactItem.js
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
-import { deleteContact } from '../../redux/contactsSlice';
+import { deleteContact } from '../../redux/operations';
 import { Container, DeleteButton } from './ContactItem.styled';
 
-export const ContactItem = ({ contact: { name, number, id } }) => {
+export const ContactItem = ({ contact: { name, phone, id } }) => {
   const dispatch = useDispatch();
 
   const handleDeleteContact = contactId => {
@@ -13,7 +13,7 @@ export const ContactItem = ({ contact: { name, number, id } }) => {
   return (
     <Container>
       <p>
-        {name}: {number}
+        {name}: {phone}
       </p>
       <DeleteButton onClick={() => handleDeleteContact(id)}>
         Delete
@@ -26,6 +26,6 @@ ContactItem.propTypes = {
   contact: PropTypes.shape({
     name: PropTypes.string.isRequired,
     id: PropTypes.string.isRequired,
-    number: PropTypes.string.isRequired,
+    phone: PropTypes.string.isRequired,
   }),
 };
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,31 +1,53 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { nanoid } from 'nanoid';
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
+import { fetchContacts, addContact, deleteContact } from './operations';
 
-const initialContactsState = [];
+const extraActions = [fetchContacts, addContact, deleteContact];
+
+const getActions = type => extraActions.map(action => action[type]);
+
+const fetchContactsSuccessReducer = (state, action) => {
+  state.items = action.payload;
+};
+
+const addContactsSuccessReducer = (state, action) => {
+  state.items.push(action.payload);
+};
+
+const deleteContactsSuccessReducer = (state, action) => {
+  const index = state.items.findIndex(task => task.id === action.payload.id);
+  state.items.splice(index, 1);
+};
+
+const pendingReducer = state => {
+  state.isLoading = true;
+};
+
+const rejectedReducer = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
+
+const fulfilledReducer = state => {
+  state.isLoading = false;
+  state.error = null;
+};
 
 const contactsSlice = createSlice({
   name: 'contacts',
-  initialState: initialContactsState,
-  reducers: {
-    addContact: {
-      reducer(state, action) {
-        return [...state, action.payload];
-      },
-      prepare(name, number) {
-        return {
-          payload: {
-            id: nanoid(),
-            name,
-            number,
-          },
-        };
-      },
-    },
-    deleteContact(state, action) {
-      return state.filter(contact => contact.id !== action.payload);
-    },
+  initialState: {
+    items: [],
+    isLoading: false,
+    error: null,
+  },
+  extraReducers: builder => {
+    builder
+      .addCase(fetchContacts.fulfilled, fetchContactsSuccessReducer)
+      .addCase(addContact.fulfilled, addContactsSuccessReducer)
+      .addCase(deleteContact.fulfilled, deleteContactsSuccessReducer)
+      .addMatcher(isAnyOf(...getActions('pending')), pendingReducer)
+      .addMatcher(isAnyOf(...getActions('rejected')), rejectedReducer)
+      .addMatcher(isAnyOf(...getActions('fulfilled')), fulfilledReducer);
   },
 });
 
-export const { addContact, deleteContact } = contactsSlice.actions;
 export const contactsReducer = contactsSlice.reducer;
